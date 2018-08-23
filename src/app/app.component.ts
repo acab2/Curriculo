aa8b45c1258920e33986735d1a7a74eb8a00ebfc
acab2 <acab2@cin.ufpe.br>
2018-08-23 02:06:49 -0300

    Dynamic incremental list of courses implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,10 +23,99 @@ export class AppComponent {
   academicFormation = '2014.2 - Cursando - Universidade Federal de Pernambuco - Ciência da Computação';
   
   title = 'hello world';
-  courses = [];
+  courses = 
+[
+	{
+		"Curso": "Programação em C/C++",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "75h"
+	},
+	{
+		"Curso": "Programação em Java",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "120h"
+	},
+	{
+		"Curso": "JavaScript(D3)",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "75h"
+	},
+	{
+		"Curso": "Haskell",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "75h"
+	},
+	{
+		"Curso": "Swift(IOS)",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "75h"
+	},
+	{
+		"Curso": "HTML",
+		"Instituicao": "Internet",
+		"Local": "Home office",
+		"CargaHoraria": "Indeterminado"
+	},
+	{
+		"Curso": "CSS",
+		"Instituicao": "Internet",
+		"Local": "Home office",
+		"CargaHoraria": "Indeterminado"
+	},
+	{
+		"Curso": "Banco de dados (Oracle)",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "75h"
+	},
+	{
+		"Curso": "Banco de dados (MySql)",
+		"Instituicao": "Faculdade Joaquim Nabuco",
+		"Local": "Paulista",
+		"CargaHoraria": "60h"
+	},
+	{
+		"Curso": "Introdução a Inteligência Artificial",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "75h"
+	},
+	{
+		"Curso": "Introdução a Redes Neurais",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "75h"
+	},
+	{
+		"Curso": "Visualização de Dados",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "75h"
+	},
+	{
+		"Curso": "Análise de requisitos",
+		"Instituicao": "Universidade Federal de Pernambuco",
+		"Local": "Recife",
+		"CargaHoraria": "75h"
+	},
+	{
+		"Curso": "Inglês",
+		"Instituicao": "SENAC",
+		"Local": "Recife",
+		"CargaHoraria": "360h"
+	}
+];
   course = "";
   
   add(){
-	  this.courses.push(this.course);
+	  this.courses.push({"Curso": this.course, "Instituicao": "padrão", "Local": "Recife", "CargaHoraria": "75h"});
   }
+  
+
+  
 }
